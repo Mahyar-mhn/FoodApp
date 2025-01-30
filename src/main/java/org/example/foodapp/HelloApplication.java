70716cccdce59a19651edb27424d781c5e9d2321
Mahyar Mohammadian <mhn.mahyarmohammadian1383@gmail.com>
2025-01-30 10:39:31 +0330

    show users for testing

--- src/main/java/org/example/foodapp/HelloApplication.java
+++ src/main/java/org/example/foodapp/HelloApplication.java
@@ -14,23 +14,24 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        // Create an instance of UserDAO
-        UserDAO userDAO = new UserDAO();
-
-        // Fetch all users
-        List<User> users = userDAO.getAllUsers();
-
-        // Print the list of users
-        System.out.println("List of Users:");
-        for (User user : users) {
-            System.out.println(user);
-        }
+//        // Create an instance of UserDAO
+//        UserDAO userDAO = new UserDAO();
+//
+//        // Fetch all users
+//        List<User> users = userDAO.getAllUsers();
+//
+//        // Print the list of users
+//        System.out.println("List of Users:");
+//        for (User user : users) {
+//            System.out.println(user);
+//        }
+        launch(args);
     }
 }
--- src/main/java/org/example/foodapp/HelloController.java
+++ src/main/java/org/example/foodapp/HelloController.java
@@ -1,14 +1,45 @@
 package org.example.foodapp;
 
+
+import DaoClasses.UserDAO;
+import Classes.User;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+
+import java.util.List;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loadButton;
+
+    @FXML
+    private ListView<String> userListView;
+
+    // Initialize method (called after FXML is loaded)
+    public void initialize() {
+        // Optionally, you can preload users here
+        // loadUsers();
+    }
+
+    // Action handler for the button
+    @FXML
+    public void onLoadButtonClick(ActionEvent event) {
+        loadUsers();
+    }
+
+    // Method to load users into the ListView
+    private void loadUsers() {
+        // Fetch users from the database
+        UserDAO userDAO = new UserDAO();
+        List<User> users = userDAO.getAllUsers();
+
+        // Clear the ListView and add user names
+        userListView.getItems().clear();
+        for (User user : users) {
+            userListView.getItems().add(user.getName());
+        }
     }
 }
